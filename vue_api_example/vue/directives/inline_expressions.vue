63eb2d85bad7048b3b29cfafd4d2c643f3bd1e0e
dhxie <xie5721072@163.com>
2015-10-13 14:45:59 +0800

    添加

---
+++ vue_api_example/vue/directives/inline_expressions.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2 class="">内联表达式</h2>
+    <pre class="brush: js">
+        <div &nbsp;v-text="message"></div>
+        function hello1111SyntaxHighlighter()
+        {
+            return "hi!";
+        }
+    </pre>
+</template>
+
+<script>
+    module.exports = {
+        data:{
+
+        },
+        ready: function(){
+            var SyntaxHighlighter = require("shCore").SyntaxHighlighter;
+            SyntaxHighlighter.highlight();
+        }
+    };
+</script>
